816dd347091024c6fc5efb25dd77154c2c76411a
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-08-03 09:17:09 +0530

    art canvas and preview pane

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title('cel7 Art Editor')
-        self.root.geometry('450x500')
+        self.root.geometry('550x600')
         self.root.resizable(False, False)
         self.root.configure(background='#505050')
         self.colors = ["#000000", "#F7F7E7", "#F70C67", "#FD9819", "#E7D50D", "#A1E119", "#44BCFF", "#AA8BFF", "#F9ACB0",
@@ -14,7 +14,14 @@ class App:
         self.pallet_canvas = tk.Canvas(self.root, bg='#ffffff')
         self.pallet_canvas.place(x=25, y=25, width=400, height=100)
 
+        self.art_canvas = tk.Canvas(self.root, bg='#ffffff')
+        self.art_canvas.place(x=65, y=135, width=420, height=420)
+
+        self.preview_canvas = tk.Canvas(self.root, bg='#ffffff')
+        self.preview_canvas.place(x=440, y=25, width=100, height=100)
+
         self.draw_pallet()
+        self.draw_art_canvas()
 
     def draw_pallet(self):
         for y in range(0, 100, 50):
@@ -30,6 +37,11 @@ class App:
         self.color = int(event.x/50) + int(event.y/50)*8
         self.draw_pallet()
         print(self.color)
+
+    def draw_art_canvas(self):
+        for y in range(7):
+            for x in range(7):
+                self.art_canvas.create_rectangle(x*60, y*60, x*60+60, y*60+60, outline="#000000", width=1)
 
 
 if __name__ == '__main__':
